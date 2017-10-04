2cd124c597c7a64714b4c5670ee7d833f9e0ce92
Jaakko Hannikainen <jgke@jgke.fi>
2017-10-04 11:49:34 +0300

    yay

--- app/Main.hs
+++ app/Main.hs
@@ -1,111 +1,49 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DataKinds                  #-}
+{-# LANGUAGE DeriveGeneric              #-}
+{-# LANGUAGE FlexibleInstances          #-}
+{-# LANGUAGE GADTs                      #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE QuasiQuotes                #-}
+{-# LANGUAGE ScopedTypeVariables        #-}
+{-# LANGUAGE TemplateHaskell            #-}
+{-# LANGUAGE TypeFamilies               #-}
+{-# LANGUAGE TypeOperators              #-}
 
 module Main where
 
-import Data.Dates (DateTime(DateTime))
-import Data.List (sort)
-import Data.Maybe (catMaybes, fromMaybe)
-import Data.Text.Template
-import qualified Data.Text as T
-import qualified Data.Text.Lazy as LT
-import Text.HTML.Scalpel
-import Text.Read (readMaybe)
+import Prelude ()
+import Prelude.Compat
 
-type Title = String
-type Link = String
+import Control.Monad.Trans.Class (lift)
+import Control.Monad.IO.Class (liftIO)
+import Network.Wai
+import Network.Wai.Handler.Warp
+import Servant
+import Lucid.Base (Html)
+import Servant.HTML.Lucid
 
-data News
-    = TextNews DateTime Link Title
-    deriving (Eq, Show, Ord)
+import Lib
 
-newsHtmlTemplate :: Template
-newsHtmlTemplate = template "<a href=\"${link}\">${date} ${title}</a><br />\n"
+type NewsApi = Get '[HTML] (Html ())
 
--- | Create 'Context' from association list.
-context :: [(T.Text, T.Text)] -> Context
-context assocs x = maybe err id . lookup x $ assocs
-  where err = error $ "Could not find key: " ++ T.unpack x
+fetchNews :: Handler (Html ())
+fetchNews = liftIO $ getNews
 
-formatDate :: DateTime -> String
-formatDate (DateTime year month day _ _ _) = sday ++ "." ++ smonth ++ "." ++ syear
-  where sday = show day
-        smonth = show month
-        syear = show year
+server :: Server NewsApi
+server = fetchNews
 
-unmarshal :: News -> [(T.Text, T.Text)]
-unmarshal (TextNews date link title) = [("date", tdate), ("link", tlink), ("title", ttitle)]
-  where tdate = T.pack $ formatDate date
-        tlink = T.pack link
-        ttitle = T.pack title
+rootApi :: Proxy NewsApi
+rootApi = Proxy
 
-newsToContext :: News -> Context
-newsToContext news = context $ unmarshal news
+app :: Application
+app = serve rootApi server
 
-showNews :: News -> LT.Text
-showNews news = render newsHtmlTemplate (newsToContext news)
--- printNews news = putStrLn $ trace (show $ unmarshal news) $ show news
-
-renderNews :: [[News]] -> LT.Text
-renderNews news = LT.concat $ map showNews (take 10 orderedNews)
-  where orderedNews = reverse $ sort $ concat news
-
-
-makeSouthernNews :: Title -> Link -> Maybe News
-makeSouthernNews title link = do
-        day <- readMaybe $ take 2 title
-        month <- readMaybe $ take 2 $ drop 3 title
-        year <- readMaybe $ take 4 $ drop 6 title
-        return $ TextNews (DateTime year month day 0 0 0) link $ drop 11 title
-
-southBaseUrl :: String
-southBaseUrl = "http://www.esavo.fi"
-
-southUrl :: String
-southUrl = southBaseUrl ++ "/tiedotteet"
-
-southernNews :: IO (Maybe [Maybe News])
-southernNews = scrapeURL southUrl allNews
-    where
-        allNews :: Scraper String [Maybe News]
-        allNews = chroots ("div" @: [hasClass "newslist-item"]) news
-
-        news :: Scraper String (Maybe News)
-        news = do
-            title <- text $ "a"
-            link <- attr "href" $ "a"
-            return $ makeSouthernNews title (southBaseUrl ++ link)
-
-makeNorthernNews :: String -> Title -> Link -> Maybe News
-makeNorthernNews date title link = do
-        day <- readMaybe $ take 2 $ drop 6 date
-        month <- readMaybe $ take 2 $ drop 4 date
-        year <- readMaybe $ take 4 date
-        return $ TextNews (DateTime year month day 0 0 0) link title
-
-northernNews :: IO (Maybe [Maybe News])
-northernNews = scrapeURL "https://www.pohjois-savo.fi/tietopalvelut/uutispoyta.html" allNews
-    where
-        allNews :: Scraper String [Maybe News]
-        allNews = chroots ("div" @: [hasClass "col-s-6"]) news
-
-        news :: Scraper String (Maybe News)
-        news = do
-            timestamp <- text $ "span" @: [hasClass "timestamp"]
-            title <- text $ ("h3" @: [hasClass "title"]) // "a"
-            link <- attr "href" $ ("h3" @: [hasClass "title"]) // "a"
-            return $ makeNorthernNews timestamp title link
-
-justifyNews :: Maybe [Maybe News] -> [News]
-justifyNews = catMaybes . fromMaybe [] 
-
-getNews :: IO String
-getNews = do
-    nnews <- northernNews
-    snews <- southernNews
-    let news = LT.unpack $ renderNews $ map justifyNews [nnews, snews]
-    return $ "<html><head></head><body><div>" ++ news ++ "</div></body></html>"
+execute :: IO ()
+execute = run 8081 app
 
 main :: IO ()
-main = do
-    news <- getNews
-    putStrLn news
+main = execute
+    -- news <- getNews
+    -- putStrLn news
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,100 @@
-module Lib
-    ( someFunc
-    ) where
+{-# LANGUAGE OverloadedStrings #-}
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+module Lib (getNews) where
+
+import Data.Dates (DateTime(DateTime))
+import Data.List (sort)
+import Data.Maybe (catMaybes, fromMaybe)
+import qualified Data.Text as T
+import qualified Data.Text.Lazy as LT
+import Text.HTML.Scalpel
+import Text.Read (readMaybe)
+import Lucid.Base
+import Lucid.Html5
+
+type Title = T.Text
+type Link = T.Text
+
+data News
+    = TextNews DateTime Link Title
+    deriving (Eq, Show, Ord)
+
+formatDate :: DateTime -> T.Text
+formatDate (DateTime year month day _ _ _) = T.pack $ sday ++ "." ++ smonth ++ "." ++ syear ++ " "
+  where sday = show day
+        smonth = show month
+        syear = show year
+
+newsToHtml :: News -> Html ()
+newsToHtml (TextNews date link title) = a_ [href_ link] $ toHtml (T.append (formatDate date) title)
+
+renderNews :: [[News]] -> [Html ()]
+renderNews news = map newsToHtml (take 10 orderedNews)
+  where orderedNews = reverse $ sort $ concat news
+
+
+makeSouthernNews :: Title -> Link -> Maybe News
+makeSouthernNews title link = do
+    day <- readMaybe $ take 2 stitle
+    month <- readMaybe $ take 2 $ drop 3 stitle
+    year <- readMaybe $ take 4 $ drop 6 stitle
+    return $ TextNews (DateTime year month day 0 0 0) link $ T.pack $ drop 11 stitle
+            where stitle = T.unpack title
+
+southBaseUrl :: String
+southBaseUrl = "http://www.esavo.fi"
+
+southUrl :: String
+southUrl = southBaseUrl ++ "/tiedotteet"
+
+southernNews :: IO (Maybe [Maybe News])
+southernNews = scrapeURL southUrl allNews
+    where
+        allNews :: Scraper String [Maybe News]
+        allNews = chroots ("div" @: [hasClass "newslist-item"]) news
+
+        news :: Scraper String (Maybe News)
+        news = do
+            title <- text $ "a"
+            link <- attr "href" $ "a"
+            return $ makeSouthernNews (T.pack title ) (T.pack (southBaseUrl ++ link))
+
+makeNorthernNews :: String -> Title -> Link -> Maybe News
+makeNorthernNews date title link = do
+        day <- readMaybe $ take 2 $ drop 6 date
+        month <- readMaybe $ take 2 $ drop 4 date
+        year <- readMaybe $ take 4 date
+        return $ TextNews (DateTime year month day 0 0 0) link title
+
+northernNews :: IO (Maybe [Maybe News])
+northernNews = scrapeURL "https://www.pohjois-savo.fi/tietopalvelut/uutispoyta.html" allNews
+    where
+        allNews :: Scraper String [Maybe News]
+        allNews = chroots ("div" @: [hasClass "col-s-6"]) news
+
+        news :: Scraper String (Maybe News)
+        news = do
+            timestamp <- text $ "span" @: [hasClass "timestamp"]
+            title <- text $ ("h3" @: [hasClass "title"]) // "a"
+            link <- attr "href" $ ("h3" @: [hasClass "title"]) // "a"
+            return $ makeNorthernNews timestamp (T.pack title) (T.pack link)
+
+justifyNews :: Maybe [Maybe News] -> [News]
+justifyNews = catMaybes . fromMaybe [] 
+
+getNewsHtml :: IO [Html ()]
+getNewsHtml = do
+      nnews <- northernNews
+      snews <- southernNews
+      return $ renderNews $ map justifyNews [nnews, snews]
+
+getNews :: IO (Html ())
+getNews = do
+    news <- getNewsHtml
+    return $ doctypehtml_ $ body_ $ mapM_ div_ news
+         
+--getNews = do
+--    nnews <- northernNews
+--    snews <- southernNews
+--    let news = LT.unpack $ renderNews $ map justifyNews [nnews, snews]
+--    return $ "<html><head></head><body><div>" ++ news ++ "</div></body></html>"
